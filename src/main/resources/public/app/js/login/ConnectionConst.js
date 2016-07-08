a010147b3e466de195dc7e2b596d3ffc2c4762c1
Félicien Clarté <clartefelicien@hotmail.com>
2016-07-08 14:27:56 +0200

    creation du squelette : controlleur, model, service,repo

--- src/main/resources/public/app/js/login/ConnectionConst.js
+++ src/main/resources/public/app/js/login/ConnectionConst.js
@@ -1,24 +1,24 @@
 (function(){
 "use strict";
 
-var basique = "http://192.168.10.19:8090";
+var basique = "http://localhost:8080";
 
 angular
 	.module('login')
 	.value('UrlConnection', {basic : basique,
 							
-							mRecherche : basique + "/resource/media.recherche",//get
-							mRechercheT : basique + "/resource/media.recherche.taille",//get
-							mAccession : basique + "/resource/media.accession",//get
-							mCreation : basique + "/resource/media.creation",//post
-							mModification : basique + "/resource/media.modification",//post
-							aRecherche : basique + "/resource/adherent.recherche",//get
-							aRechercheT : basique + "/resource/adherent.recherche.taille",//get
-							aAccession : basique + "/resource/adherent.accession",//get
-							aCreation : basique + "/resource/adherent.creation",//post
-							aModification : basique + "/resource/adherent.modification",//post
-							eAjout : basique + "/resource/emprunt.ajout",//post
-							cLogin : basique + "/resource/connexion.login",//post
-							cRight : basique + "/resource/connexion.right",});//get
+							mRecherche : basique + "/api/media.recherche",//get
+							mRechercheT : basique + "/api/media.recherche.taille",//get
+							mAccession : basique + "/api/media.accession",//get
+							mCreation : basique + "/api/media.creation",//post
+							mModification : basique + "/api/media.modification",//post
+							aRecherche : basique + "/api/adherent.recherche",//get
+							aRechercheT : basique + "/api/adherent.recherche.taille",//get
+							aAccession : basique + "/api/adherent.accession",//get
+							aCreation : basique + "/api/adherent.creation",//post
+							aModification : basique + "/api/adherent.modification",//post
+							eAjout : basique + "/api/emprunt.ajout",//post
+							cLogin : basique + "/api/connexion.login",//post
+							cRight : basique + "/api/connexion.right",});//get
 							
 })()
